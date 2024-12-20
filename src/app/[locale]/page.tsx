29ff14f0b77f45bb390ce29abddea925a050a0a8
liaoyi <2417276459@qq.com>
2024-12-20 15:17:06 +0800

    feat(i18n): use next-intl :sparkles:

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,18 @@
 import { StoreDemo } from '@/components/example/store'
 import DarkModeToggle from '@/components/toggles/darkmode'
+import { LanguageToggle } from '@/components/toggles/language'
+import { useTranslations } from 'next-intl'
 
 export default function Home() {
+  const t = useTranslations()
   return (
     <div className='flex h-screen items-center justify-center'>
       <div className='flex flex-col items-center gap-6'>
         <StoreDemo />
-        <div className='flex items-center gap-4'>
+
+        <div className='flex w-full items-center justify-center gap-4'>
+          {t('hello')}
+          <LanguageToggle />
           <DarkModeToggle />
         </div>
       </div>
